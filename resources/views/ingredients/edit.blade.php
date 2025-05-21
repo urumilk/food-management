b730fe5e95f4823c7c0f3bde388809733276d4be
urumilk <urumilk23810@gmail.com>
2025-05-21 17:21:51 +0900

    賞味期限のrequiredを削除

--- resources/views/ingredients/edit.blade.php
+++ resources/views/ingredients/edit.blade.php
@@ -4,7 +4,7 @@
     @csrf
     @method('PATCH')
     <input type="text" name="name" placeholder="食材名" required value="{{ old('name', $ingredient->name) }}">
-    <input type="date" name="expiration_date" required value="{{ old('expiration_date', $ingredient->expiration_date) }}">
+    <input type="date" name="expiration_date" value="{{ old('expiration_date', $ingredient->expiration_date) }}">
     <button type="submit">更新</button>
 </form>
 @endsection
