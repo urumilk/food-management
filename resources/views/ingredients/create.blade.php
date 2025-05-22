fa61a87ab8373b4cd51cb82e3fd20ded10fcf6a0
urumilk <urumilk23810@gmail.com>
2025-05-22 13:11:44 +0900

    登録ボタン変更

--- resources/views/ingredients/create.blade.php
+++ resources/views/ingredients/create.blade.php
@@ -3,10 +3,16 @@
 @section('title', '登録 -' . config('app.name'))
 
 @section('content')
-<form action="{{ route('ingredients.store') }}" method="POST">
+<form action="{{ route('ingredients.store') }}" method="POST" class="flex items-center space-x-2">
     @csrf
-    <input type="text" name="name" placeholder="食材名">
-    <input type="date" name="expiration_date">
-    <button type="submit">登録</button>
+    <input type="text" name="name" placeholder="食材名" class="border px-2 py-1 h-10 border px-3 rounded">
+    <input type="date" name="expiration_date" class="border px-2 py-1 h-10 border px-3 rounded">
+    <button type="submit" class="group flex h-10 w-10 select-none items-center justify-center rounded-lg border border-zinc-100 bg-white leading-8 text-zinc-950 shadow-[0_-1px_0_0px_#d4d4d8_inset,0_0_0_1px_#f4f4f5_inset,0_0.5px_0_1.5px_#fff_inset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]" aria-label="Change language">
+        <span class="flex items-center group-active:[transform:translate3d(0,1px,0)]">
+            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-zinc-950">
+                <path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+            </svg>
+        </span>
+    </button>
 </form>
 @endsection
